3e2efeb8b998aeb66e5a62e000958d2ecdd2dd05
davescriven <unknown@unknown.com>
2008-04-14 08:08:13 +0000

    Adding missing project file.

---
+++ DataTypes/SvgCoordinateUnits.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Svg
+{
+    /// <summary>
+    /// Defines the various coordinate units certain SVG elements may use.
+    /// </summary>
+    public enum SvgCoordinateUnits
+    {
+        /// <summary>
+        /// Indicates that the coordinate system of the entire document is to be used.
+        /// </summary>
+        UserSpaceOnUse,
+        /// <summary>
+        /// Indicates that the coordinate system of the owner element is to be used.
+        /// </summary>
+        ObjectBoundingBox
+    }
+}
