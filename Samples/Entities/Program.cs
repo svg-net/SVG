42d67f71a9ba4c3c20ae41a1ed7dfd829e26754f
davescriven <unknown@unknown.com>
2010-05-06 10:45:36 +0000

    - Adding an XML entities based sample

---
+++ Samples/Entities/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Svg;
+using System.IO;
+
+namespace Entities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\sample.svg");
+
+            var sampleDoc = SvgDocument.Open(filePath, new Dictionary<string, string> 
+                {
+                    {"entity1", "fill:red" },
+                    {"entity2", "fill:yellow" }
+                });
+
+            sampleDoc.Draw().Save(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\sample.png"));
+        }
+    }
+}
