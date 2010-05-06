ac0384ae5ccb8db71c9a0b6e17c174c4285e390d
davescriven <unknown@unknown.com>
2010-05-06 10:07:13 +0000

    - Adding runtime style changing example.

---
+++ Samples/RuntimeUpdates/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Svg;
+using System.IO;
+using System.Drawing;
+
+namespace SvgSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sampleDoc = SvgDocument.Open(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\sample.svg"));
+            sampleDoc.GetElementById<SvgUse>("Commonwealth_Star").Fill = new SvgColourServer(Color.Black);
+            sampleDoc.Draw().Save(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\sample.png"));
+        }
+    }
+}
