2cb11ac17ef6dcd8b28297c928683de666884e30
tebjan <tebjan@vvvv.org>
2015-09-17 15:40:53 +0200

    added signing and public token for friend assembly

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -32,9 +32,14 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.0.0.*")]
+[assembly: AssemblyVersion("2.1.0.*")]
 //[assembly: AssemblyFileVersion("1.0.1.*")]
 
 [assembly: CLSCompliant(true)]
 
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Svg.UnitTests")]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Svg.UnitTests,PublicKey=" + 
+    "00240000048000009400000006020000002400005253413100040000010001008d4e723a8c76be" + 
+"e667607d1fca2c0f0cdcc1c1b926ae46669128282ecad43e6d0776497cd8289dca11e4479773d5" +
+"45fc4c557686de548aadbb8652fa550e21d4c402885fec4c1deebfa79e861adb966fc8f4e78235" +
+"79a535280ddd3a0168cb4d19522c7591b6693377058675da70e50c7bd6fdceae055cef085f02a0" +
+"5a7f0cb4")]
