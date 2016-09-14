c53691093f20b3fd9be7b1edfaa38ebfc3bda177
sovietmagic <sovietmagic@hotmail.com>
2016-09-14 15:29:17 +0300

    Unit test added for SvgPointCollection.ToString method

---
+++ Tests/Svg.UnitTests/SvgPointCollectionTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Svg.UnitTests {
+
+    [TestClass]
+    public class SvgPointCollectionTests 
+    {
+
+        [TestMethod]
+        public void ToStringReturnsValidString() 
+        {
+            var collection = new SvgPointCollection 
+            {
+                new SvgUnit(1.6f), new SvgUnit(3.2f),
+                new SvgUnit(1.2f), new SvgUnit(5f)
+            };
+            Assert.AreEqual("1.6,3.2 1.2,5", collection.ToString());
+        }
+    }
+}
