cfab3774b4aa8b465e75dda2b67af69e22a4c046
Gertjan van Heertum <git@netietsmeer.nl>
2019-01-08 16:25:53 +0100

    Updated tests for #399
    Updated the test suite to test the various important/hex combinations and preparation for the 3 char hexes (at least the testing).

--- Tests/Svg.UnitTests/LexerIssueTests.cs
+++ Tests/Svg.UnitTests/LexerIssueTests.cs
@@ -17,9 +17,10 @@ namespace Svg.UnitTests
 	public class LexerIssueTests : SvgTestHelper
 	{
 		private const string ResourceStringEmptyDTagFile = "Issue399_LexerIssue.EmptyDTag.svg";
-		private const string ResourceStringStyleWithImportantInBorderFile = "Issue399_LexerIssue.LexerFailOnImportantInBorder.svg";
-		private const string ResourceStringStyleWithoutImportantInBorderFile = "Issue399_LexerIssue.LexerSuccesOnNoImportantInBorder.svg";
-		
+		private const string ResourceStringStyleWithImportantAfterHex = "Issue399_LexerIssue.LexerTestImportantAfterHex.svg";
+		private const string ResourceStringStyleWithImportantAfterNonHex = "Issue399_LexerIssue.LexerTestImportantAfterNonHex.svg";
+		private const string ResourceStringStyleWithInvalidHex = "Issue399_LexerIssue.LexerTestFailOnInvalidHex.svg";
+	
 		/// <summary>
 		/// We encountered issues in the example file that were caused by an empty d tag in some of the elements
 		/// </summary>
@@ -31,23 +32,38 @@ namespace Svg.UnitTests
 		}
 
 		/// <summary>
-		/// Stylesheet lexer fails if there is an important in the border styling
+		/// Stylesheet lexer fails if there is an important after a hex value, this tests the 3 and 6 lenght variants in the file
 		/// </summary>
 		[TestMethod]
-		public void Lexer_ImportantInBorderStyle_Success()
+		public void Lexer_ImportantAfterHex_Success()
 		{
-			var xml = GetXMLDocFromResource(GetFullResourceString(ResourceStringStyleWithImportantInBorderFile));
+			var xml = GetXMLDocFromResource(GetFullResourceString(ResourceStringStyleWithImportantAfterHex));
 			var doc = OpenSvg(xml);
 		}
 
 		/// <summary>
-		///Reference test for border style without important (should never fail)
+		///Reference test if there is an important after a non-hex value (should never fail) and on hex without an important
 		/// </summary>
 		[TestMethod]
-		public void Lexer_NoImportantInBorderStyle_Success()
+		public void Lexer_NoImportantAndImportantAfterNonHex_Success()
 		{
-			var xml = GetXMLDocFromResource(GetFullResourceString(ResourceStringStyleWithoutImportantInBorderFile));
+			var xml = GetXMLDocFromResource(GetFullResourceString(ResourceStringStyleWithImportantAfterNonHex));
 			var doc = OpenSvg(xml);
 		}
+
+		[TestMethod]
+		public void Lexer_FileWithInvalidHex_FailsWithError()
+		{
+			try
+			{
+				var xml = GetXMLDocFromResource(GetFullResourceString(ResourceStringStyleWithInvalidHex));
+				var doc = OpenSvg(xml);
+				Assert.Fail("Expected the xml to fail due to invalid HEX value");
+			}
+			catch (ArgumentOutOfRangeException)
+			{
+				//Success, we expected this
+			}
+		}	
 	}
 }
