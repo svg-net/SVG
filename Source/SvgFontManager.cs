abc957d7d8aec67e5af20decdf8654936969a4e7
garyranson <gary.ranson@peractosystems.com>
2019-02-04 10:21:32 +0000

    Add annotations to SvgFontManager also renamed test case

--- Source/SvgFontManager.cs
+++ Source/SvgFontManager.cs
@@ -4,10 +4,15 @@ using System.Drawing;
 using System.Drawing.Text;
 using System.IO;
 using System.Linq;
-using System.Text.RegularExpressions;
 
 namespace Svg
 {
+    /// <summary>
+    /// Manages access to <see cref="SystemFonts"/> and any privately loaded fonts.
+    /// When a font is requested in the render process, if the font is not found as an embedded SvgFont, the render
+    /// process will SvgFontManager.FindFont method.
+    /// </summary>
+
     public static class SvgFontManager
     {
         private static readonly Dictionary<string, FontFamily> SystemFonts;
@@ -17,7 +22,12 @@ namespace Svg
             SystemFonts = FontFamily.Families.ToDictionary(ff => ff.Name.ToLower());
         }
 
-        public static FontFamily AddFontFile(string path)
+        /// <summary>
+        /// Loads a font from the given path.
+        /// </summary>
+        /// <param name="path">A <see cref="string"/> containing the full path to the font file.</param>
+        /// <returns>An <see cref="FontFamily"/> of the loaded font.</returns>
+        public static FontFamily LoadFontFamily(string path)
         {
             var pfc = new PrivateFontCollection();
             var fp = Path.GetFullPath(path);
@@ -25,6 +35,15 @@ namespace Svg
             return pfc.Families.Length == 0 ? null : pfc.Families[0];
         }
 
+        /// <summary>
+        /// This method searches a dictionary of fonts (pre loaded with the system fonts). If a
+        /// font can't be found and a callback has been provided - then the callback should perform
+        /// any validation and return a font (or null if not found/error).
+        /// Where a font can't be located it is the responsibility of the caller to perform any
+        /// exception handling.
+        /// </summary>
+        /// <param name="name">A <see cref="string"/> containing the FamilyName of the font.</param>
+        /// <returns>An <see cref="FontFamily"/> of the loaded font or null is not located.</returns>
         public static FontFamily FindFont(string name)
         {
             if (name == null) return null;
