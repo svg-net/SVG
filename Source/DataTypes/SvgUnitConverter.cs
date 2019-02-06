feb39612b558b0f3c04ebd03ad83d0c030aa1566
garyranson <gary.ranson@peractosystems.com>
2019-02-06 16:14:36 +0000

    Updates to support multiple dotnet targets
    - support v3.5, v4.0, v4.5.2, v4.7.2 and dotnetcore2.2
    - added support for Appveyor build
    - added explict FrameworkPathOverride to projects
    - added <IsTextProject> to csproj file
    - remove net35 from unittests. Updated licence method.

--- Source/DataTypes/SvgUnitConverter.cs
+++ Source/DataTypes/SvgUnitConverter.cs
@@ -1,11 +1,6 @@
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Diagnostics;
 using System.Globalization;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Web.UI.WebControls;
 
 namespace Svg
 {
