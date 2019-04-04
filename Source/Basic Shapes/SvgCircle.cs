8d147c1058926904d1ad1d6c8b3116e909b51ea4
H1Gdev <h1g.z.ouroboros+github@gmail.com>
2019-04-04 13:25:23 +0900

    Small refactor in SvgCircle.

--- Source/Basic Shapes/SvgCircle.cs
+++ Source/Basic Shapes/SvgCircle.cs
@@ -9,10 +9,6 @@ namespace Svg
     public class SvgCircle : SvgPathBasedElement
     {
         private GraphicsPath _path;
-        
-        private SvgUnit _radius;
-        private SvgUnit _centerX;
-        private SvgUnit _centerY;
 
         /// <summary>
         /// Gets the center point of the circle.
@@ -26,46 +22,22 @@ namespace Svg
         [SvgAttribute("cx")]
         public virtual SvgUnit CenterX
         {
-            get { return this._centerX; }
-            set
-            {
-            	if(_centerX != value)
-            	{
-            		this._centerX = value;
-            		this.IsPathDirty = true;
-            		OnAttributeChanged(new AttributeEventArgs{ Attribute = "cx", Value = value });
-            	}
-            }
+            get { return this.Attributes.GetAttribute<SvgUnit>("cx"); }
+            set { this.Attributes["cx"] = value; this.IsPathDirty = true; }
         }
 
         [SvgAttribute("cy")]
         public virtual SvgUnit CenterY
         {
-        	get { return this._centerY; }
-        	set
-        	{
-        		if(_centerY != value)
-        		{
-        			this._centerY = value;
-        			this.IsPathDirty = true;
-        			OnAttributeChanged(new AttributeEventArgs{ Attribute = "cy", Value = value });
-        		}
-        	}
+            get { return this.Attributes.GetAttribute<SvgUnit>("cy"); }
+            set { this.Attributes["cy"] = value; this.IsPathDirty = true; }
         }
 
         [SvgAttribute("r")]
         public virtual SvgUnit Radius
         {
-        	get { return this._radius; }
-        	set
-        	{
-        		if(_radius != value)
-        		{
-        			this._radius = value;
-        			this.IsPathDirty = true;
-        			OnAttributeChanged(new AttributeEventArgs{ Attribute = "r", Value = value });
-        		}
-        	}
+            get { return this.Attributes.GetAttribute<SvgUnit>("r"); }
+            set { this.Attributes["r"] = value; this.IsPathDirty = true; }
         }
 
         /// <summary>
@@ -75,21 +47,21 @@ namespace Svg
         {
             if (this._path == null || this.IsPathDirty)
             {
-							float halfStrokeWidth = base.StrokeWidth / 2;
+                var halfStrokeWidth = base.StrokeWidth / 2;
 
-							// If it is to render, don't need to consider stroke width.
-							// i.e stroke width only to be considered when calculating boundary
-							if (renderer != null)
-							{
-								halfStrokeWidth = 0;
-								this.IsPathDirty = false;
-							}
+                // If it is to render, don't need to consider stroke width.
+                // i.e stroke width only to be considered when calculating boundary
+                if (renderer != null)
+                {
+                    halfStrokeWidth = 0;
+                    this.IsPathDirty = false;
+                }
 
                 _path = new GraphicsPath();
                 _path.StartFigure();
-								var center = this.Center.ToDeviceValue(renderer, this);
-								var radius = this.Radius.ToDeviceValue(renderer, UnitRenderingType.Other, this) + halfStrokeWidth;
-								_path.AddEllipse(center.X - radius, center.Y - radius, 2 * radius, 2 * radius);
+                var center = this.Center.ToDeviceValue(renderer, this);
+                var radius = this.Radius.ToDeviceValue(renderer, UnitRenderingType.Other, this) + halfStrokeWidth;
+                _path.AddEllipse(center.X - radius, center.Y - radius, 2 * radius, 2 * radius);
                 _path.CloseFigure();
             }
             return _path;
@@ -106,15 +78,6 @@ namespace Svg
             {
                 base.Render(renderer);
             }
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="SvgCircle"/> class.
-        /// </summary>
-        public SvgCircle()
-        {
-            CenterX = new SvgUnit(0.0f);
-            CenterY = new SvgUnit(0.0f);
         }
 
 
