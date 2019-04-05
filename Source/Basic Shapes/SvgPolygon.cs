0eafd9bc54cb8b384f97939ed968f2b90fee3295
H1Gdev <h1g.z.ouroboros+github@gmail.com>
2019-04-05 15:14:44 +0900

    Small refactor in SvgPolygon.

--- Source/Basic Shapes/SvgPolygon.cs
+++ Source/Basic Shapes/SvgPolygon.cs
@@ -1,5 +1,5 @@
-using System.Drawing.Drawing2D;
 using System.Diagnostics;
+using System.Drawing.Drawing2D;
 
 namespace Svg
 {
@@ -10,14 +10,14 @@ namespace Svg
     public class SvgPolygon : SvgMarkerElement
     {
         private GraphicsPath _path;
-        
+
         /// <summary>
         /// The points that make up the SvgPolygon
         /// </summary>
         [SvgAttribute("points")]
         public SvgPointCollection Points
         {
-            get { return this.Attributes["points"] as SvgPointCollection; }
+            get { return this.Attributes.GetAttribute<SvgPointCollection>("points"); }
             set { this.Attributes["points"] = value; this.IsPathDirty = true; }
         }
 
@@ -35,13 +35,13 @@ namespace Svg
                     {
                         var endPoint = SvgUnit.GetDevicePoint(points[i], points[i + 1], renderer, this);
 
-                      // If it is to render, don't need to consider stroke width.
+                        // If it is to render, don't need to consider stroke width.
                         // i.e stroke width only to be considered when calculating boundary
                         if (renderer == null)
                         {
-                          var radius = base.StrokeWidth / 2;
-                          _path.AddEllipse(endPoint.X - radius, endPoint.Y - radius, 2 * radius, 2 * radius);
-                          continue;
+                            var radius = base.StrokeWidth / 2;
+                            _path.AddEllipse(endPoint.X - radius, endPoint.Y - radius, 2 * radius, 2 * radius);
+                            continue;
                         }
 
                         //first line
@@ -62,7 +62,7 @@ namespace Svg
 
                 this._path.CloseFigure();
                 if (renderer != null)
-                  this.IsPathDirty = false;
+                    this.IsPathDirty = false;
             }
             return this._path;
         }
