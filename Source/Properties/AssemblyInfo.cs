43f5accbffdf156fd2aa052ac8522fd48551a3f3
tebjan <tebjan@vvvv.org>
2019-05-07 16:22:34 +0200

    released nuget 2.4.3

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.4.2.*")]
+[assembly: AssemblyVersion("2.4.3.*")]
 //[assembly: AssemblyFileVersion("1.0.1.*")]
 
 [assembly: CLSCompliant(true)]
