dc57aa1793bb48144bf42a7e5aa7056bb164b382
Sören Kuklau <soeren@kuklau.name>
2019-05-09 20:31:27 +0200

    Don't use Windows-specific calls if not on Windows
    - don't use Win32-specific interop calls to determine the DPI
      if not on Windows
    - possible fix for #427
    - add a barebones unit test for this
    - note that even the Windows code isn't correct as soon as you have multiple screens
    - on macOS, causes 20 instead of 5 unit tests to pass

---
+++ Tests/Svg.UnitTests/DpiTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Svg.UnitTests
+{
+    /// <summary>
+    /// Test that basic graphics operations work. Currently only supported
+    /// on Windows, macOS, and Linux.
+    /// </summary>
+    [TestClass]
+    public class DpiTest
+    {
+        /// <summary>
+        /// We should get a valid dpi (probably 72, 96 or similar).
+        /// </summary>
+        [TestMethod]
+        public void TestDpiAboveZero()
+        {
+            Assert.IsTrue(SvgDocument.PointsPerInch > 0);
+        }
+    }
+}
