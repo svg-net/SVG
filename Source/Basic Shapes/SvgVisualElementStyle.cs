15e429319e6f69e1fd038dc7a53b0d11cb74939a
H1Gdev <h1g.z.ouroboros+github@gmail.com>
2019-06-12 16:47:10 +0900

    Small refactoring in SvgVisualElement.

--- Source/Basic Shapes/SvgVisualElementStyle.cs
+++ Source/Basic Shapes/SvgVisualElementStyle.cs
@@ -1,12 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Text;
-using System.Reflection;
 using System.ComponentModel;
-using Svg.DataTypes;
-using System.Text.RegularExpressions;
-using System.Linq;
 
 namespace Svg
 {
@@ -37,14 +29,7 @@ namespace Svg
         // Displayable - false if attribute display="none", true otherwise
         protected virtual bool Displayable
         {
-            get
-            {
-                string checkForDisplayNone = this.Attributes["display"] as string;
-                if ((!string.IsNullOrEmpty(checkForDisplayNone)) && (checkForDisplayNone == "none"))
-                    return false;
-                else
-                    return true;
-            }
+            get { return !string.Equals(Display, "none"); }
         }
 
         /// <summary>
@@ -56,6 +41,5 @@ namespace Svg
             get { return this.Attributes["enable-background"] as string; }
             set { this.Attributes["enable-background"] = value; }
         }
-
     }
 }
