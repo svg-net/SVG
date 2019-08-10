0026867aa18657d6928db4790e6028d34a2c5469
Gertjan <git@netietsmeer.nl>
2019-08-10 17:41:03 +0200

    Added attributes to the SvgScript element Added testing for the attributes

--- Source/Document Structure/SvgScript.cs
+++ Source/Document Structure/SvgScript.cs
@@ -9,9 +9,36 @@ namespace Svg
     [SvgElement("script")]
     public class SvgScript : SvgElement
     {
+
+        private string _scriptType;
+
+        [SvgAttribute("type")]
+        public string ScriptType  //TODO: Enum?
+        { 
+            get { return _scriptType; } 
+            set { _scriptType = value; } 
+        }
+
+        private string _crossOrigin;
+
+        [SvgAttribute("crossorigin")]
+        public string CrossOrigin 
+        {
+            get { return _crossOrigin; }
+            set { _crossOrigin = value; }
+        }
+
+        private string _href;
+        [SvgAttribute("href")]
+        public string Href //TODO: URI? 
+        {
+            get { return _href; }
+            set { _href = value; }
+        }
+
         public override SvgElement DeepCopy()
         {
-            throw new System.NotImplementedException();
+            return DeepCopy<SvgScript>();        
         }
     }
 }
--- Tests/Svg.UnitTests/SvgJavascriptTests.cs
+++ Tests/Svg.UnitTests/SvgJavascriptTests.cs
@@ -15,37 +15,74 @@ namespace Svg.UnitTests
     {
         protected override string TestResource { get { return GetFullResourceString("ScriptTag.TestFile.svg"); } } 
 
-        [Test]
-        public void SvgDocument_ReadFileWithScript_YieldsNoError()
+        /// <summary>
+        /// Helper to retrieve the script element from the test document (expected in the root as type SvgScript with the elementname script)
+        /// Will assert the element not being null and of the correct type
+        /// </summary>
+        private SvgScript GetScriptElementFromDocument(SvgDocument doc)
         {
-            //Expect to read a document with JS without problem
+            var el = doc.Children.FirstOrDefault(c => c.ElementName == "script");
+            Assert.IsNotNull(el, "Could not find the script element");
+            Assert.IsAssignableFrom(typeof(SvgScript), el, "Script tag was not correctly typed, expected SvgScript as type");
+            return el as SvgScript;
+        }
+
+        /// <summary>
+        /// Check if a file with a script tag can correctly be read (and the types are as expected)
+        /// </summary>
+        [Test]
+        public void SvgDocumentWithSvgScript_ReadFile_ScriptTagReadFromSvgInCorrectType()
+        {
             var doc = SvgDocument.Open(GetXMLDocFromResource(TestResource));
             Assert.IsNotNull(doc);
+            
             var circleElement = doc.Children.FirstOrDefault(c => c.ElementName == "circle");
             Assert.IsNotNull(circleElement, "Expected to find the circle element in the document");
             Assert.IsAssignableFrom(typeof(SvgCircle), circleElement, "Expected the found circle element to be of type SvgCircle");
             
-            var scriptElement = doc.Children.FirstOrDefault(c => c.ElementName == "script");
+            //Check if the type is correctly parsed
+            var scriptElement = GetScriptElementFromDocument(doc);
             Assert.IsNotNull(scriptElement, "Expected to find the script element in the document");
-            Assert.IsAssignableFrom(typeof(SvgScript), scriptElement, "Expected the found script element to be of type SvgScript");            
         }
 
+        /// <summary>
+        /// Test whether the attributes are parsed correctly (for now we only test the scripttype)
+        /// </summary>
         [Test]
-        public void SvgDocument_FileWithScript_CanRetrieveScriptTag()
+        public void SvgDocumentWithSvgScript_AttributeParsing_CorrectTypeAttribute()
         {
-            //Expected to retrieve the script tag(s) from the document
+            var doc = SvgDocument.Open(GetXMLDocFromResource(TestResource));
+            Assert.IsNotNull(doc);
+            
+            var tag = GetScriptElementFromDocument(doc);
+            Assert.AreEqual("text/javascript", tag.ScriptType, "Expected the type attribute to be read");
+        }
+
+        /// <summary>
+        /// Retrieve the script content, this is not expected to be escaped
+        /// </summary>
+        [Test]
+        public void SvgDocumentWithSvgScript_RetrieveScriptContent_ScriptContentIsNotEscaped()
+        {
             Assert.Inconclusive();
         }
 
+        /// <summary>
+        /// Try to add a script to a document and check if the content was added correctly
+        /// and escaped as expected.
+        /// </summary>
         [Test]
-        public void SvgDocument_AddScript_AddScriptWithEscapingToSvg()
+        public void SvgDocumentWithSvgScript_AddScript_EscapintIsAdded()
         {
             //Script is expected to be escaped
             Assert.Inconclusive();
         }
 
+        /// <summary>
+        /// Test if altering a script (reading, changing and reinserting) does not yield odd results.
+        /// </summary>
         [Test]
-        public void SvgDocument_AlterScriptTag_HandlesEscapingCorrectly()
+        public void SvgDocumentWithSvgScript_AlterScriptTag_HandlesEscapingCorrectly()
         {
             //Expecting to get the script without CDATA
             //After update expect the updated script with CDATA in the results
