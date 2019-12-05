90ea2d297df2197f2a21abf1fd3063f36be1410f
H1Gdev <h1g.z.ouroboros+github@gmail.com>
2019-12-05 12:52:52 +0900

    Implement ToString method in SvgPathSegmentList.

--- Source/Paths/SvgPathBuilder.cs
+++ Source/Paths/SvgPathBuilder.cs
@@ -255,29 +255,5 @@ namespace Svg
 
             return base.ConvertFrom(context, culture, value);
         }
-
-        public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
-        {
-            if (destinationType == typeof(string))
-            {
-                var paths = value as SvgPathSegmentList;
-
-                if (paths != null)
-                {
-                    var s = string.Join(" ", paths.Select(p => p.ToString()).ToArray());
-                    return s;
-                }
-            }
-
-            return base.ConvertTo(context, culture, value, destinationType);
-        }
-
-        public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType)
-        {
-            if (destinationType == typeof(string))
-                return true;
-
-            return base.CanConvertTo(context, destinationType);
-        }
     }
 }
