7745a20ed4f39f05d0906ba91e9ee65e2b272af1
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-01-18 23:27:56 +0100

    Added SvgFlood properties

--- Source/Filter Effects/feFlood/SvgFlood.cs
+++ Source/Filter Effects/feFlood/SvgFlood.cs
@@ -3,6 +3,20 @@
     [SvgElement("feFlood")]
     public class SvgFlood : SvgFilterPrimitive
     {
+        [SvgAttribute("flood-color")]
+        public virtual SvgPaintServer FloodColor
+        {
+            get { return GetAttribute("flood-color", true, SvgPaintServer.NotSet); }
+            set { Attributes["flood-color"] = value; }
+        }
+
+        [SvgAttribute("flood-opacity")]
+        public virtual float FloodOpacity
+        {
+            get { return GetAttribute("flood-opacity", true, 1f); }
+            set { Attributes["flood-opacity"] = FixOpacityValue(value); }
+        }
+
         public override void Process(ImageBuffer buffer)
         {
             // TODO: Implement feFlood filter Process().
@@ -11,6 +25,16 @@
         public override SvgElement DeepCopy()
         {
             return DeepCopy<SvgFlood>();
+        }
+
+        public override SvgElement DeepCopy<T>()
+        {
+            var newObj = base.DeepCopy<T>() as SvgFlood;
+
+            newObj.FloodColor = FloodColor;
+            newObj.FloodOpacity = FloodOpacity;
+
+            return newObj;
         }
     }
 }
