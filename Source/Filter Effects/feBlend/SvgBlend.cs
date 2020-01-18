6b4d6e6bb589bbaef01c953747071cdaf678ae66
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-01-18 22:06:32 +0100

    Added BlendMode and Input2 properties

--- Source/Filter Effects/feBlend/SvgBlend.cs
+++ Source/Filter Effects/feBlend/SvgBlend.cs
@@ -3,6 +3,22 @@
     [SvgElement("feBlend")]
     public class SvgBlend : SvgFilterPrimitive
     {
+        private SvgBlendMode _mode = SvgBlendMode.Normal;
+
+        [SvgAttribute("mode")]
+        public SvgBlendMode BlendMode
+        {
+            get { return _mode; }
+            set { _mode = value; Attributes["mode"] = value; }
+        }
+
+        [SvgAttribute("in2")]
+        public string Input2
+        {
+            get { return GetAttribute<string>("in2", false); }
+            set { Attributes["in2"] = value; }
+        }
+
         public override void Process(ImageBuffer buffer)
         {
             // TODO: Implement feBlend filter Process().
@@ -10,7 +26,17 @@
 
         public override SvgElement DeepCopy()
         {
-            return DeepCopy<SvgBlend>();
+            return DeepCopy<SvgGaussianBlur>();
+        }
+
+        public override SvgElement DeepCopy<T>()
+        {
+            var newObj = base.DeepCopy<T>() as SvgBlend;
+
+            newObj._mode = _mode;
+            newObj.Input2 = Input2;
+
+            return newObj;
         }
     }
 }
