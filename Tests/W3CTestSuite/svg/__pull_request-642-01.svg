svg-net/SVG

commit 2a38deb8cf03d336f1d3bc8d1fc877656fd95638

CoordinateParser should handle NumState.invalid first (#640)
* CoordinateParser should handle NumState.invalid first
* Added test images
* Update ReleaseNotes.md
* Apply EditorConfig settings

Source/Paths/CoordinateParser.cs
@@ -274,6 +274,12 @@ namespace Svg
                         break;
                 }
 
+                if (_newState == NumState.invalid)
+                {
+                    result = float.MinValue;
+                    return MarkState(false);
+                }
+
                 if (_newState < _currState)
                 {
                     result = float.Parse(_coords.Substring(Position, i - Position), NumberStyles.Float, CultureInfo.InvariantCulture);
@@ -286,11 +292,6 @@ namespace Svg
                     Position = i;
                 }
 
-                if (_newState == NumState.invalid)
-                {
-                    result = float.MinValue;
-                    return MarkState(false);
-                }
                 _currState = _newState;
                 ++i;
             }

Tests/W3CTestSuite/svg/__pull_request-640-01.svg
@@ -1,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<svg id="svg1" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+  <title>Stop processing on invalid data</title>
+  <desc>`text` and everything after it should be ignored</desc>
+
+  <!-- should be covered -->
+  <path id="path1" d="M 20 40 L 160 180 L 30 150" fill="none" stroke="red"/>
+
+  <polyline id="polyline1" points="20 40 160 180 30 150 text" fill="none" stroke="green"/>
+
+  <!-- image frame -->
+  <rect id="frame" x="1" y="1" width="198" height="198" fill="none" stroke="black"/>
+</svg>

Tests/W3CTestSuite/svg/__pull_request-640-02.svg
@@ -1,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<svg id="svg1" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+  <title>Stop processing on invalid data</title>
+  <desc>`text` and everything after it should be ignored</desc>
+
+  <!-- should be covered -->
+  <path id="path1" d="M 20 40 L 160 180 L 30 150" fill="red" stroke="red"/>
+
+  <polygon id="polygon1" points="20 40 160 180 30 150 text" fill="green" stroke="green"/>
+
+  <!-- image frame -->
+  <rect id="frame" x="1" y="1" width="198" height="198" fill="none" stroke="black"/>
+</svg>

commit edd647df8dbe28ee275dcc5587a9aec50aed4260

Fix CoordinateParser (#642)
* Fixed regression introduce by #640 * Added tests

Source/Paths/CoordinateParser.cs
@@ -274,13 +274,7 @@ namespace Svg
                         break;
                 }
 
-                if (_newState == NumState.invalid)
-                {
-                    result = float.MinValue;
-                    return MarkState(false);
-                }
-
-                if (_newState < _currState)
+                if (_currState != NumState.separator && _newState < _currState)
                 {
                     result = float.Parse(_coords.Substring(Position, i - Position), NumberStyles.Float, CultureInfo.InvariantCulture);
                     Position = i;
@@ -292,6 +286,11 @@ namespace Svg
                     Position = i;
                 }
 
+                if (_newState == NumState.invalid)
+                {
+                    result = float.MinValue;
+                    return MarkState(false);
+                }
                 _currState = _newState;
                 ++i;
             }

Tests/W3CTestSuite/svg/__pull_request-642-01.svg
@@ -1,0 +1,11 @@
+<svg id="svg-root" width="100%" height="100%" viewBox="0 0 480 360" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <g id="test-body-content">
+    <g fill="gold">
+      <rect x="220" y="280" width="160" height="8"/>
+    </g>
+    <g transform="translate(60,-30) scale(8,2)">
+      <path d="M 20 160 H 40" stroke-width="2" stroke="red" />
+      <path d="M 20 160 H 40#90" stroke-width="4" stroke="black" />
+    </g>
+  </g>
+</svg>