2046f8fd332f51a51e7308597a8a6bf6501a0748
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-01-22 21:52:02 +0100

    Added SvgMorphologyOperator enum

---
+++ Source/Filter Effects/feMorphology/SvgMorphologyOperator.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+namespace Svg.FilterEffects
+{
+    [TypeConverter(typeof(EnumBaseConverter<SvgMorphologyOperator>))]
+    public enum SvgMorphologyOperator
+    {
+        Erode,
+        Dilate
+    }
+}
