c4c20be4438ad8ee68e1f9090aed657af981fff8
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-01-22 22:01:08 +0100

    Added SvgTurbulenceType enum

---
+++ Source/Filter Effects/feTurbulence/SvgTurbulenceType.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+namespace Svg.FilterEffects
+{
+    [TypeConverter(typeof(EnumBaseConverter<SvgTurbulenceType>))]
+    public enum SvgTurbulenceType
+    {
+        FractalNoise,
+        Turbulence
+    }
+}
