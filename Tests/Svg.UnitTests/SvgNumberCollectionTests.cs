7a13a758756a058dea76b6648b239a67ca62bf57
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-01-22 20:32:29 +0100

    Added SvgNumberCollection class

---
+++ Tests/Svg.UnitTests/SvgNumberCollectionTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace Svg.UnitTests
+{
+    [TestFixture]
+    public class SvgNumberCollectionTests
+    {
+        [Test]
+        public void ToStringReturnsValidString()
+        {
+            var collection = new SvgNumberCollection
+            {
+                1.6f, 3.2f,
+                1.2f, 5f
+            };
+            Assert.AreEqual("1.6 3.2 1.2 5", collection.ToString());
+        }
+    }
+}
