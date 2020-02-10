3efa867fb69ee2c68a59f5c1e8b92c988e94e1c7
H1Gdev <h1g.z.ouroboros+github@gmail.com>
2020-02-11 05:07:57 +0900

    Remove DefaultValue in EnumConverters. (#668)
    * Remove Inherit in SvgCoordinateUnits.

--- Source/DataTypes/EnumConverters.cs
+++ Source/DataTypes/EnumConverters.cs
@@ -32,29 +32,16 @@ namespace Svg
         /// <summary>Defines if the enum literal shall be converted to camelCase, lowercase or kebab-case.</summary>
         public CaseHandling CaseHandlingMode { get; }
 
-        /// <summary>If specified, upon conversion, the default value will result in 'null'.</summary>
-        public T? DefaultValue { get; protected set; }
-
         /// <summary>Creates a new instance.</summary>
-        /// <param name="defaultValue">Specified the default value of the enum.</param>
         /// <param name="caseHandling">Defines if the value shall be converted to camelCase, lowercase or kebab-case.</param>
-        public EnumBaseConverter(T defaultValue, CaseHandling caseHandling = CaseHandling.CamelCase)
+        public EnumBaseConverter(CaseHandling caseHandling = CaseHandling.CamelCase)
         {
-            DefaultValue = defaultValue;
             CaseHandlingMode = caseHandling;
         }
 
         /// <summary>Attempts to convert the provided value to <typeparamref name="T"/>.</summary>
         public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
         {
-            if (value == null)
-            {
-                if (DefaultValue.HasValue)
-                    return DefaultValue.Value;
-
-                return Activator.CreateInstance(typeof(T));
-            }
-
             if (value is string stringValue)
             {
                 if (CaseHandlingMode == CaseHandling.KebabCase)
@@ -72,10 +59,6 @@ namespace Svg
         {
             if (destinationType == typeof(string) && value is T)
             {
-                // If the value id the default value, no need to write the attribute.
-                if (DefaultValue.HasValue && Equals(value, DefaultValue.Value))
-                    return null;
-
                 var stringValue = ((T)value).ToString();
                 if (CaseHandlingMode == CaseHandling.CamelCase)
                     return string.Format("{0}{1}", stringValue[0].ToString().ToLower(), stringValue.Substring(1));
@@ -92,110 +75,61 @@ namespace Svg
 
     public sealed class SvgFillRuleConverter : EnumBaseConverter<SvgFillRule>
     {
-        public SvgFillRuleConverter() : base(SvgFillRule.NonZero, CaseHandling.LowerCase) { }
+        public SvgFillRuleConverter() : base(CaseHandling.LowerCase) { }
     }
 
-    public sealed class SvgColourInterpolationConverter : EnumBaseConverter<SvgColourInterpolation>
-    {
-        public SvgColourInterpolationConverter() : base(SvgColourInterpolation.SRGB) { }
-    }
+    public sealed class SvgColourInterpolationConverter : EnumBaseConverter<SvgColourInterpolation> { }
 
     public sealed class SvgClipRuleConverter : EnumBaseConverter<SvgClipRule>
     {
-        public SvgClipRuleConverter() : base(SvgClipRule.NonZero, CaseHandling.LowerCase) { }
+        public SvgClipRuleConverter() : base(CaseHandling.LowerCase) { }
     }
 
-    public sealed class SvgTextAnchorConverter : EnumBaseConverter<SvgTextAnchor>
-    {
-        public SvgTextAnchorConverter() : base(SvgTextAnchor.Start) { }
-    }
+    public sealed class SvgTextAnchorConverter : EnumBaseConverter<SvgTextAnchor> { }
 
-    public sealed class SvgStrokeLineCapConverter : EnumBaseConverter<SvgStrokeLineCap>
-    {
-        public SvgStrokeLineCapConverter() : base(SvgStrokeLineCap.Butt) { }
-    }
+    public sealed class SvgStrokeLineCapConverter : EnumBaseConverter<SvgStrokeLineCap> { }
 
-    public sealed class SvgStrokeLineJoinConverter : EnumBaseConverter<SvgStrokeLineJoin>
-    {
-        public SvgStrokeLineJoinConverter() : base(SvgStrokeLineJoin.Miter) { }
-    }
+    public sealed class SvgStrokeLineJoinConverter : EnumBaseConverter<SvgStrokeLineJoin> { }
 
-    public sealed class SvgMarkerUnitsConverter : EnumBaseConverter<SvgMarkerUnits>
-    {
-        public SvgMarkerUnitsConverter() : base(SvgMarkerUnits.StrokeWidth) { }
-    }
+    public sealed class SvgMarkerUnitsConverter : EnumBaseConverter<SvgMarkerUnits> { }
 
-    public sealed class SvgFontStyleConverter : EnumBaseConverter<SvgFontStyle>
-    {
-        public SvgFontStyleConverter() : base(SvgFontStyle.Normal) { }
-    }
+    public sealed class SvgFontStyleConverter : EnumBaseConverter<SvgFontStyle> { }
 
-    public sealed class SvgOverflowConverter : EnumBaseConverter<SvgOverflow>
-    {
-        public SvgOverflowConverter() : base(SvgOverflow.Auto) { }
-    }
+    public sealed class SvgOverflowConverter : EnumBaseConverter<SvgOverflow> { }
 
-    public sealed class SvgTextLengthAdjustConverter : EnumBaseConverter<SvgTextLengthAdjust>
-    {
-        public SvgTextLengthAdjustConverter() : base(SvgTextLengthAdjust.Spacing) { }
-    }
+    public sealed class SvgTextLengthAdjustConverter : EnumBaseConverter<SvgTextLengthAdjust> { }
 
-    public sealed class SvgTextPathMethodConverter : EnumBaseConverter<SvgTextPathMethod>
-    {
-        public SvgTextPathMethodConverter() : base(SvgTextPathMethod.Align) { }
-    }
+    public sealed class SvgTextPathMethodConverter : EnumBaseConverter<SvgTextPathMethod> { }
 
-    public sealed class SvgTextPathSpacingConverter : EnumBaseConverter<SvgTextPathSpacing>
-    {
-        public SvgTextPathSpacingConverter() : base(SvgTextPathSpacing.Exact) { }
-    }
+    public sealed class SvgTextPathSpacingConverter : EnumBaseConverter<SvgTextPathSpacing> { }
 
-    public sealed class SvgShapeRenderingConverter : EnumBaseConverter<SvgShapeRendering>
-    {
-        public SvgShapeRenderingConverter() : base(SvgShapeRendering.Inherit) { }
-    }
+    public sealed class SvgShapeRenderingConverter : EnumBaseConverter<SvgShapeRendering> { }
 
-    public sealed class SvgTextRenderingConverter : EnumBaseConverter<SvgTextRendering>
-    {
-        public SvgTextRenderingConverter() : base(SvgTextRendering.Inherit) { }
-    }
+    public sealed class SvgTextRenderingConverter : EnumBaseConverter<SvgTextRendering> { }
 
-    public sealed class SvgImageRenderingConverter : EnumBaseConverter<SvgImageRendering>
-    {
-        public SvgImageRenderingConverter() : base(SvgImageRendering.Inherit) { }
-    }
+    public sealed class SvgImageRenderingConverter : EnumBaseConverter<SvgImageRendering> { }
 
     public sealed class SvgFontVariantConverter : EnumBaseConverter<SvgFontVariant>
     {
-        public SvgFontVariantConverter() : base(SvgFontVariant.Normal, CaseHandling.KebabCase) { }
+        public SvgFontVariantConverter() : base(CaseHandling.KebabCase) { }
     }
 
-    public sealed class SvgCoordinateUnitsConverter : EnumBaseConverter<SvgCoordinateUnits>
-    {
-        // TODO Inherit is not actually valid. See TODO on SvgCoordinateUnits enum.
-        public SvgCoordinateUnitsConverter() : base(SvgCoordinateUnits.Inherit) { }
-    }
+    public sealed class SvgCoordinateUnitsConverter : EnumBaseConverter<SvgCoordinateUnits> { }
 
-    public sealed class SvgGradientSpreadMethodConverter : EnumBaseConverter<SvgGradientSpreadMethod>
-    {
-        public SvgGradientSpreadMethodConverter() : base(SvgGradientSpreadMethod.Pad) { }
-    }
+    public sealed class SvgGradientSpreadMethodConverter : EnumBaseConverter<SvgGradientSpreadMethod> { }
 
     public sealed class SvgTextDecorationConverter : EnumBaseConverter<SvgTextDecoration>
     {
-        public SvgTextDecorationConverter() : base(SvgTextDecoration.None, CaseHandling.KebabCase) { }
+        public SvgTextDecorationConverter() : base(CaseHandling.KebabCase) { }
     }
 
     public sealed class SvgFontStretchConverter : EnumBaseConverter<SvgFontStretch>
     {
-        public SvgFontStretchConverter() : base(SvgFontStretch.Normal, CaseHandling.KebabCase) { }
+        public SvgFontStretchConverter() : base(CaseHandling.KebabCase) { }
     }
 
     public sealed class SvgFontWeightConverter : EnumBaseConverter<SvgFontWeight>
     {
-        // TODO Defaulting to Normal although it should be All if this is used on a font face.
-        public SvgFontWeightConverter() : base(SvgFontWeight.Normal) { }
-
         public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
         {
             if (value is string)
@@ -215,6 +149,7 @@ namespace Svg
             }
             return base.ConvertFrom(context, culture, value);
         }
+
         public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
         {
             if (destinationType == typeof(string) && value is SvgFontWeight)
@@ -236,10 +171,7 @@ namespace Svg
         }
     }
 
-    public sealed class SvgTextTransformationConverter : EnumBaseConverter<SvgTextTransformation>
-    {
-        public SvgTextTransformationConverter() : base(SvgTextTransformation.None) { }
-    }
+    public sealed class SvgTextTransformationConverter : EnumBaseConverter<SvgTextTransformation> { }
 
     public static class Enums
     {
