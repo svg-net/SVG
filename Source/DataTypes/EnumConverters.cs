db7ff2427bab0c61607faea0e9749051654de6de
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-02-12 21:35:23 +0100

    Revert to default argument style

--- Source/DataTypes/EnumConverters.cs
+++ Source/DataTypes/EnumConverters.cs
@@ -35,13 +35,8 @@ namespace Svg
         public CaseHandling CaseHandlingMode { get; }
 
         /// <summary>Creates a new instance.</summary>
-        public EnumBaseConverter() : this(CaseHandling.CamelCase)
-        {
-        }
-
-        /// <summary>Creates a new instance.</summary>
         /// <param name="caseHandling">Defines if the value shall be converted to camelCase, lowercase or kebab-case.</param>
-        public EnumBaseConverter(CaseHandling caseHandling)
+        public EnumBaseConverter(CaseHandling caseHandling = CaseHandling.CamelCase)
         {
             CaseHandlingMode = caseHandling;
         }
