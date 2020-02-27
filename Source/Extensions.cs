a306ffdbad518ad848526f3e045a9e88e02733d1
H1Gdev <h1g.z.ouroboros+github@gmail.com>
2020-02-27 13:56:29 +0900

    Fix: #675 'Endless loop and out of memory on a specific file' issue.

--- Source/Extensions.cs
+++ Source/Extensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Svg
 {
@@ -29,33 +30,21 @@ namespace Svg
 
         private static IEnumerable<SvgElement> GetDescendants<T>(IEnumerable<T> source, bool self) where T : SvgElement
         {
-            var positons = new Stack<int>();
-            int currPos;
-            SvgElement currParent;
-            foreach (var start in source)
+            foreach (var top in source)
             {
-                if (start != null)
+                if (top == null)
+                    continue;
+
+                if (self)
+                    yield return top;
+
+                var elements = new Stack<SvgElement>(top.Children.Reverse());
+                while (elements.Count > 0)
                 {
-                    if (self) yield return start;
-
-                    positons.Push(0);
-                    currParent = start;
-
-                    while (positons.Count > 0)
-                    {
-                        currPos = positons.Pop();
-                        if (currPos < currParent.Children.Count)
-                        {
-                            yield return currParent.Children[currPos];
-                            currParent = currParent.Children[currPos];
-                            positons.Push(currPos + 1);
-                            positons.Push(0);
-                        }
-                        else
-                        {
-                            currParent = currParent.Parent;
-                        }
-                    }
+                    var element = elements.Pop();
+                    yield return element;
+                    foreach (var e in element.Children.Reverse())
+                        elements.Push(e);
                 }
             }
             yield break;
