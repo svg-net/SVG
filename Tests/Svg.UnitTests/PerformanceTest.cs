125e5b644ee5fb35f57f9b4303e60c5fb6c9c7df
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2021-01-07 21:15:02 +0100

    Add UnitConverter profiler unit test

--- Tests/Svg.UnitTests/PerformanceTest.cs
+++ Tests/Svg.UnitTests/PerformanceTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO.Compression;
 using System.IO;
 using System.Text;
@@ -60,6 +61,42 @@ namespace Svg.UnitTests
                     throw;
                 }
             }
+        }
+
+        private static readonly SvgUnitConverter UnitConverter = new SvgUnitConverter();
+
+        [Test]
+        public void UnitConverterProfiler()
+        {
+            UnitConverter.ConvertFrom(null, CultureInfo.InvariantCulture, "1pt");
+            UnitConverter.ConvertFrom(null, CultureInfo.InvariantCulture, "1.25px");
+            UnitConverter.ConvertFrom(null, CultureInfo.InvariantCulture, "1pc");
+            UnitConverter.ConvertFrom(null, CultureInfo.InvariantCulture, "15px");
+            UnitConverter.ConvertFrom(null, CultureInfo.InvariantCulture, "1mm");
+            UnitConverter.ConvertFrom(null, CultureInfo.InvariantCulture, "3.543307px");
+            UnitConverter.ConvertFrom(null, CultureInfo.InvariantCulture, "1cm");
+            UnitConverter.ConvertFrom(null, CultureInfo.InvariantCulture, "35.43307px");
+            UnitConverter.ConvertFrom(null, CultureInfo.InvariantCulture, "1in");
+            UnitConverter.ConvertFrom(null, CultureInfo.InvariantCulture, "90px");
+            UnitConverter.ConvertFrom(null, CultureInfo.InvariantCulture, "15em");
+            UnitConverter.ConvertFrom(null, CultureInfo.InvariantCulture, "0.2822222mm");
+            UnitConverter.ConvertFrom(null, CultureInfo.InvariantCulture, "3990");
+            UnitConverter.ConvertFrom(null, CultureInfo.InvariantCulture, "1990");
+            UnitConverter.ConvertFrom(null, CultureInfo.InvariantCulture, "-50");
+            UnitConverter.ConvertFrom(null, CultureInfo.InvariantCulture, ".4in");
+            UnitConverter.ConvertFrom(null, CultureInfo.InvariantCulture, ".25em");
+            UnitConverter.ConvertFrom(null, CultureInfo.InvariantCulture, "10%");
+            UnitConverter.ConvertFrom(null, CultureInfo.InvariantCulture, "1%");
+            UnitConverter.ConvertFrom(null, CultureInfo.InvariantCulture, "0%");
+            UnitConverter.ConvertFrom(null, CultureInfo.InvariantCulture, "100%");
+            UnitConverter.ConvertFrom(null, CultureInfo.InvariantCulture, "1.2em");
+            UnitConverter.ConvertFrom(null, CultureInfo.InvariantCulture, "medium");
+            UnitConverter.ConvertFrom(null, CultureInfo.InvariantCulture, "x-small");
+            UnitConverter.ConvertFrom(null, CultureInfo.InvariantCulture, "xx-large");
+            UnitConverter.ConvertFrom(null, CultureInfo.InvariantCulture, "657.45");
+            UnitConverter.ConvertFrom(null, CultureInfo.InvariantCulture, "12.5");
+            UnitConverter.ConvertFrom(null, CultureInfo.InvariantCulture, "0");
+            UnitConverter.ConvertFrom(null, CultureInfo.InvariantCulture, "12");
         }
     }
 }
