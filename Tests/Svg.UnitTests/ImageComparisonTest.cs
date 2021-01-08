ecc08e3c60ef28e92a7b9cf7e564684c16a56241
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-01-08 10:32:32 +0000

    Use GetEntryAssembly

--- Tests/Svg.UnitTests/ImageComparisonTest.cs
+++ Tests/Svg.UnitTests/ImageComparisonTest.cs
@@ -345,7 +345,7 @@ namespace Svg.UnitTests
             get
             {
                 if (_basePath != null) return _basePath;
-                var basePath = AppDomain.CurrentDomain.BaseDirectory;
+                var basePath = System.Reflection.Assembly.GetEntryAssembly().CodeBase;
                 while (!basePath.ToLower().EndsWith("svg"))
                 {
                     basePath = Path.GetDirectoryName(basePath);
