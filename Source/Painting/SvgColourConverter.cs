58de8fd74c8aa9a1fc287f8635cb7a95600da07f
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2021-01-12 19:38:03 +0100

    Update SvgColourConverter.cs

--- Source/Painting/SvgColourConverter.cs
+++ Source/Painting/SvgColourConverter.cs
@@ -3,6 +3,7 @@ using System.ComponentModel;
 using System.Drawing;
 using System.Globalization;
 using System.Text;
+using Svg.Helpers;
 
 namespace Svg
 {
@@ -11,6 +12,287 @@ namespace Svg
     /// </summary>
     public class SvgColourConverter : ColorConverter
     {
+#if true
+        private static readonly char[] SplitChars = new char[] { ',' , ' ' };
+
+        public object Parse(ITypeDescriptorContext context, CultureInfo culture, ReadOnlySpan<char> colour)
+        {
+            colour = colour.Trim();
+
+            // RGB support
+            if (colour.IndexOf("rgb".AsSpan()) == 0)
+            {
+                try
+                {
+                    int start = colour.IndexOf('(') + 1;
+                    int length = colour.IndexOf(')') - start;
+
+                    //get the values from the RGB string
+                    string[] values = colour.Substring(start, length).Split(SplitChars, StringSplitOptions.RemoveEmptyEntries);
+
+                    //determine the alpha value if this is an RGBA (it will be the 4th value if there is one)
+                    int alphaValue = 255;
+                    if (values.Length > 3)
+                    {
+                        //the alpha portion of the rgba is not an int 0-255 it is a decimal between 0 and 1
+                        //so we have to determine the corosponding byte value
+                        var alphastring = values[3];
+                        if (alphastring.StartsWith(".", StringComparison.InvariantCulture))
+                        {
+                            alphastring = "0" + alphastring;
+                        }
+
+                        var alphaDecimal = decimal.Parse(alphastring, CultureInfo.InvariantCulture);
+
+                        if (alphaDecimal <= 1)
+                        {
+                            alphaValue = (int) Math.Round(alphaDecimal * 255);
+                        }
+                        else
+                        {
+                            alphaValue = (int) Math.Round(alphaDecimal);
+                        }
+                    }
+
+                    Color colorpart;
+                    if (values[0].Trim().EndsWith("%", StringComparison.InvariantCulture))
+                    {
+                        colorpart = Color.FromArgb(alphaValue,
+                            (int) Math.Round(255 * float.Parse(values[0].Trim().TrimEnd('%'), NumberStyles.Any,
+                                CultureInfo.InvariantCulture) / 100f),
+                            (int) Math.Round(255 * float.Parse(values[1].Trim().TrimEnd('%'), NumberStyles.Any,
+                                CultureInfo.InvariantCulture) / 100f),
+                            (int) Math.Round(255 * float.Parse(values[2].Trim().TrimEnd('%'), NumberStyles.Any,
+                                CultureInfo.InvariantCulture) / 100f));
+                    }
+                    else
+                    {
+                        colorpart = Color.FromArgb(alphaValue, int.Parse(values[0], CultureInfo.InvariantCulture),
+                            int.Parse(values[1], CultureInfo.InvariantCulture),
+                            int.Parse(values[2], CultureInfo.InvariantCulture));
+                    }
+
+                    return colorpart;
+                }
+                catch
+                {
+                    throw new SvgException("Colour is in an invalid format: '" + colour.ToString() + "'");
+                }
+            }
+
+            // HSL support
+            if (colour.IndexOf("hsl".AsSpan()) == 0)
+            {
+                try
+                {
+                    int start = colour.IndexOf('(') + 1;
+                    int length = colour.IndexOf(')') - start;
+
+                    //get the values from the RGB string
+
+                    var parts = new StringSplitEnumerator(colour.Slice(start, length), SplitChars.AsSpan());
+
+
+
+
+                    string[] values = colour.Substring(start, length).Split(SplitChars, StringSplitOptions.RemoveEmptyEntries);
+
+                    if (values[1].EndsWith("%", StringComparison.InvariantCulture))
+                    {
+                        values[1] = values[1].TrimEnd('%');
+                    }
+
+                    if (values[2].EndsWith("%", StringComparison.InvariantCulture))
+                    {
+                        values[2] = values[2].TrimEnd('%');
+                    }
+
+
+
+
+                    // Get the HSL values in a range from 0 to 1.
+                    double h = double.Parse(values[0], CultureInfo.InvariantCulture) / 360.0;
+                    double s = double.Parse(values[1], CultureInfo.InvariantCulture) / 100.0;
+                    double l = double.Parse(values[2], CultureInfo.InvariantCulture) / 100.0;
+
+                    // Convert the HSL color to an RGB color
+                    Color colorpart = Hsl2Rgb(h, s, l);
+                    return colorpart;
+                }
+                catch
+                {
+                    throw new SvgException("Colour is in an invalid format: '" + colour.ToString() + "'");
+                }
+            }
+
+            // HEX support
+            if (colour.IndexOf('#') == 0 && colour.Length == 4)
+            {
+                // TODO: Optimize
+                var hex = string.Format(culture, "#{0}{0}{1}{1}{2}{2}", colour[1], colour[2], colour[3]);
+                return base.ConvertFrom(context, culture, hex);
+            }
+
+            // SystemColors support
+            if (colour.Equals("activeborder".AsSpan(), StringComparison.InvariantCultureIgnoreCase))
+            {
+                return SystemColors.ActiveBorder;
+            }
+            if (colour.Equals("activecaption".AsSpan(), StringComparison.InvariantCultureIgnoreCase))
+            {
+                return SystemColors.ActiveCaption;
+            }
+            if (colour.Equals("appworkspace".AsSpan(), StringComparison.InvariantCultureIgnoreCase))
+            {
+                return SystemColors.AppWorkspace;
+            }
+            if (colour.Equals("background".AsSpan(), StringComparison.InvariantCultureIgnoreCase))
+            {
+                return SystemColors.Desktop;
+            }
+            if (colour.Equals("buttonface".AsSpan(), StringComparison.InvariantCultureIgnoreCase))
+            {
+                return SystemColors.Control;
+            }
+            if (colour.Equals("buttonhighlight".AsSpan(), StringComparison.InvariantCultureIgnoreCase))
+            {
+                return SystemColors.ControlLightLight;
+            }
+            if (colour.Equals("buttonshadow".AsSpan(), StringComparison.InvariantCultureIgnoreCase))
+            {
+                return SystemColors.ControlDark;
+            }
+            if (colour.Equals("buttontext".AsSpan(), StringComparison.InvariantCultureIgnoreCase))
+            {
+                return SystemColors.ControlText;
+            }
+            if (colour.Equals("captiontext".AsSpan(), StringComparison.InvariantCultureIgnoreCase))
+            {
+                return SystemColors.ActiveCaptionText;
+            }
+            if (colour.Equals("graytext".AsSpan(), StringComparison.InvariantCultureIgnoreCase))
+            {
+                return SystemColors.GrayText;
+            }
+            if (colour.Equals("highlight".AsSpan(), StringComparison.InvariantCultureIgnoreCase))
+            {
+                return SystemColors.Highlight;
+            }
+            if (colour.Equals("highlighttext".AsSpan(), StringComparison.InvariantCultureIgnoreCase))
+            {
+                return SystemColors.HighlightText;
+            }
+            if (colour.Equals("inactiveborder".AsSpan(), StringComparison.InvariantCultureIgnoreCase))
+            {
+                return SystemColors.InactiveBorder;
+            }
+            if (colour.Equals("inactivecaption".AsSpan(), StringComparison.InvariantCultureIgnoreCase))
+            {
+                return SystemColors.InactiveCaption;
+            }
+            if (colour.Equals("inactivecaptiontext".AsSpan(), StringComparison.InvariantCultureIgnoreCase))
+            {
+                return SystemColors.InactiveCaptionText;
+            }
+            if (colour.Equals("infobackground".AsSpan(), StringComparison.InvariantCultureIgnoreCase))
+            {
+                return SystemColors.Info;
+            }
+            if (colour.Equals("infotext".AsSpan(), StringComparison.InvariantCultureIgnoreCase))
+            {
+                return SystemColors.InfoText;
+            }
+            if (colour.Equals("menu".AsSpan(), StringComparison.InvariantCultureIgnoreCase))
+            {
+                return SystemColors.Menu;
+            }
+            if (colour.Equals("menutext".AsSpan(), StringComparison.InvariantCultureIgnoreCase))
+            {
+                return SystemColors.MenuText;
+            }
+            if (colour.Equals("scrollbar".AsSpan(), StringComparison.InvariantCultureIgnoreCase))
+            {
+                return SystemColors.ScrollBar;
+            }
+            if (colour.Equals("threeddarkshadow".AsSpan(), StringComparison.InvariantCultureIgnoreCase))
+            {
+                return SystemColors.ControlDarkDark;
+            }
+            if (colour.Equals("threedface".AsSpan(), StringComparison.InvariantCultureIgnoreCase))
+            {
+                return SystemColors.Control;
+            }
+            if (colour.Equals("threedhighlight".AsSpan(), StringComparison.InvariantCultureIgnoreCase))
+            {
+                return SystemColors.ControlLight;
+            }
+            if (colour.Equals("threedlightshadow".AsSpan(), StringComparison.InvariantCultureIgnoreCase))
+            {
+                return SystemColors.ControlLightLight;
+            }
+            if (colour.Equals("window".AsSpan(), StringComparison.InvariantCultureIgnoreCase))
+            {
+                return SystemColors.Window;
+            }
+            if (colour.Equals("windowframe".AsSpan(), StringComparison.InvariantCultureIgnoreCase))
+            {
+                return SystemColors.WindowFrame;
+            }
+            if (colour.Equals("windowtext".AsSpan(), StringComparison.InvariantCultureIgnoreCase))
+            {
+                return SystemColors.WindowText;
+            }
+
+            // Numbers are handled as colors by System.Drawing.ColorConverter - we
+            // have to prevent this and ignore the color instead (see #342).
+#if NETSTANDARD2_1 || NETCORE || NETCOREAPP2_2 || NETCOREAPP3_0
+            if (int.TryParse(colour, NumberStyles.Integer, CultureInfo.InvariantCulture, out _))
+            {
+                return SvgPaintServer.NotSet;
+            }
+#else
+            if (int.TryParse(colour.ToString(), NumberStyles.Integer, CultureInfo.InvariantCulture, out _))
+            {
+                return SvgPaintServer.NotSet;
+            }
+#endif
+            var index = colour.LastIndexOf("grey".AsSpan(), StringComparison.InvariantCultureIgnoreCase);
+            if (index >= 0 && index + 4 == colour.Length)
+            {
+                var gray = new StringBuilder(colour)
+                    .Replace("grey", "gray", index, 4)
+                    .Replace("Grey", "Gray", index, 4)
+                    .ToString();
+
+                return base.ConvertFrom(context, culture, gray);
+            }
+
+            return base.ConvertFrom(context, culture, colour);
+        }
+
+        /// <summary>
+        /// Converts the given object to the converter's native type.
+        /// </summary>
+        /// <param name="context">A <see cref="T:System.ComponentModel.TypeDescriptor"/> that provides a format context. You can use this object to get additional information about the environment from which this converter is being invoked.</param>
+        /// <param name="culture">A <see cref="T:System.Globalization.CultureInfo"/> that specifies the culture to represent the color.</param>
+        /// <param name="value">The object to convert.</param>
+        /// <returns>
+        /// An <see cref="T:System.Object"/> representing the converted value.
+        /// </returns>
+        /// <exception cref="T:System.ArgumentException">The conversion cannot be performed.</exception>
+        /// <PermissionSet>
+        ///     <IPermission class="System.Security.Permissions.FileIOPermission, mscorlib, Version=2.0.3600.0, Culture=neutral, PublicKeyToken=b77a5c561934e089" version="1" Unrestricted="true"/>
+        /// </PermissionSet>
+        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        {
+            if (value is string colour)
+            {
+                return Parse(context, culture, colour);
+            }
+
+            return base.ConvertFrom(context, culture, value);
+        }
+#else
         public object Parse(ITypeDescriptorContext context, CultureInfo culture, string colour)
         {
             colour = colour.Trim();
@@ -192,6 +474,7 @@ namespace Svg
 
             return base.ConvertFrom(context, culture, value);
         }
+#endif
 
         public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
         {
