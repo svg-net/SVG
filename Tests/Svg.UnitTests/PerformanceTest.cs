16ea392242b7bff0103ee6fa305c6c9f6adf40b9
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2021-01-14 19:17:26 +0100

    Revert

--- Tests/Svg.UnitTests/PerformanceTest.cs
+++ Tests/Svg.UnitTests/PerformanceTest.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO.Compression;
 using System.IO;
+using System.Text;
+using System.Reflection;
+
 using NUnit.Framework;
-using Svg.Transforms;
 
 namespace Svg.UnitTests
 {
