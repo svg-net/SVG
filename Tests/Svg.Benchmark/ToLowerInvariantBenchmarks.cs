d0cfb90cad53cb92d82d3ce94c9e5e2520ad0538
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2021-01-15 21:01:34 +0100

    Fix ToLowerAscii

--- Tests/Svg.Benchmark/ToLowerInvariantBenchmarks.cs
+++ Tests/Svg.Benchmark/ToLowerInvariantBenchmarks.cs
@@ -45,42 +45,78 @@ namespace Svg.Benchmark
             }
         }
 
-        private static void ToLowerAscii(string colour, ref Span<char> buffer)
+        public static void ToLowerAscii(in ReadOnlySpan<char> colour, ref Span<char> buffer)
         {
             for (int i = 0; i < colour.Length; i++)
             {
                 var c = colour[i];
-                if (c >= 'a' && c <= 'z')
+                switch (c)
                 {
-                    buffer[i] = (char)(c - 32);
-                }
-                else if(c >= 'A' && c <= 'Z')
-                {
-                    buffer[i] = (char)(c + 32);
-                }
-                else
-                {
-                    buffer[i] = c;
+                    case 'A': buffer[i] = 'a'; break;
+                    case 'B': buffer[i] = 'b'; break;
+                    case 'C': buffer[i] = 'c'; break;
+                    case 'D': buffer[i] = 'd'; break;
+                    case 'E': buffer[i] = 'e'; break;
+                    case 'F': buffer[i] = 'f'; break;
+                    case 'G': buffer[i] = 'g'; break;
+                    case 'H': buffer[i] = 'h'; break;
+                    case 'I': buffer[i] = 'i'; break;
+                    case 'J': buffer[i] = 'j'; break;
+                    case 'K': buffer[i] = 'k'; break;
+                    case 'L': buffer[i] = 'l'; break;
+                    case 'M': buffer[i] = 'm'; break;
+                    case 'N': buffer[i] = 'n'; break;
+                    case 'O': buffer[i] = 'o'; break;
+                    case 'P': buffer[i] = 'p'; break;
+                    case 'Q': buffer[i] = 'q'; break;
+                    case 'R': buffer[i] = 'r'; break;
+                    case 'S': buffer[i] = 's'; break;
+                    case 'T': buffer[i] = 't'; break;
+                    case 'U': buffer[i] = 'u'; break;
+                    case 'V': buffer[i] = 'v'; break;
+                    case 'W': buffer[i] = 'w'; break;
+                    case 'X': buffer[i] = 'x'; break;
+                    case 'Y': buffer[i] = 'y'; break;
+                    case 'Z': buffer[i] = 'z'; break;
+                    default: buffer[i] = c; break;
                 }
             }
         }
 
-        private static void ToLowerAscii(in ReadOnlySpan<char> colour, ref Span<char> buffer)
+        public static void ToLowerAscii(string colour, ref Span<char> buffer)
         {
             for (int i = 0; i < colour.Length; i++)
             {
                 var c = colour[i];
-                if (c >= 'a' && c <= 'z')
+                switch (c)
                 {
-                    buffer[i] = (char)(c - 32);
-                }
-                else if(c >= 'A' && c <= 'Z')
-                {
-                    buffer[i] = (char)(c + 32);
-                }
-                else
-                {
-                    buffer[i] = c;
+                    case 'A': buffer[i] = 'a'; break;
+                    case 'B': buffer[i] = 'b'; break;
+                    case 'C': buffer[i] = 'c'; break;
+                    case 'D': buffer[i] = 'd'; break;
+                    case 'E': buffer[i] = 'e'; break;
+                    case 'F': buffer[i] = 'f'; break;
+                    case 'G': buffer[i] = 'g'; break;
+                    case 'H': buffer[i] = 'h'; break;
+                    case 'I': buffer[i] = 'i'; break;
+                    case 'J': buffer[i] = 'j'; break;
+                    case 'K': buffer[i] = 'k'; break;
+                    case 'L': buffer[i] = 'l'; break;
+                    case 'M': buffer[i] = 'm'; break;
+                    case 'N': buffer[i] = 'n'; break;
+                    case 'O': buffer[i] = 'o'; break;
+                    case 'P': buffer[i] = 'p'; break;
+                    case 'Q': buffer[i] = 'q'; break;
+                    case 'R': buffer[i] = 'r'; break;
+                    case 'S': buffer[i] = 's'; break;
+                    case 'T': buffer[i] = 't'; break;
+                    case 'U': buffer[i] = 'u'; break;
+                    case 'V': buffer[i] = 'v'; break;
+                    case 'W': buffer[i] = 'w'; break;
+                    case 'X': buffer[i] = 'x'; break;
+                    case 'Y': buffer[i] = 'y'; break;
+                    case 'Z': buffer[i] = 'z'; break;
+                    default: buffer[i] = c; break;
                 }
             }
         }
