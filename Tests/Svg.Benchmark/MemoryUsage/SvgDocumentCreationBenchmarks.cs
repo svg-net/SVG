04f2ec647c39da186fb6cbbf88499de73b96c45e
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2021-01-16 23:29:14 +0100

    Added SvgDocument_new_FromSvg_Empty

--- Tests/Svg.Benchmark/MemoryUsage/SvgDocumentCreationBenchmarks.cs
+++ Tests/Svg.Benchmark/MemoryUsage/SvgDocumentCreationBenchmarks.cs
@@ -4,10 +4,18 @@ namespace Svg.Benchmark
 {
     public class SvgDocumentCreationBenchmarks
     {
-        [Benchmark]
+        [Benchmark(Baseline = true)]
         public void SvgDocument_new()
         {
             var result = new SvgDocument();
+        }
+
+        private static string EmptySvg = @"<svg xmlns='http://www.w3.org/2000/svg'></svg>";
+
+        [Benchmark]
+        public void SvgDocument_new_FromSvg_Empty()
+        {
+            var doc = SvgDocument.FromSvg<SvgDocument>(EmptySvg);
         }
     }
 }
