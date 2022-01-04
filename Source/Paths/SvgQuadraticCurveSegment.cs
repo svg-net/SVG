svg-net/SVG

commit c9c98ff55dbb30b1f90cc0b9d842053040b30572

Support relative commands in Paths (#925)
* Remove Start property in SvgPathSegment class.
* Support relative commands.
* Add tests.

Source/Paths/SvgQuadraticCurveSegment.cs
@@ -7,42 +7,74 @@ namespace Svg.Pathing
     {
         public PointF ControlPoint { get; set; }
 
-        private PointF FirstControlPoint
-        {
-            get
-            {
-                var x1 = Start.X + (ControlPoint.X - Start.X) * 2 / 3;
-                var y1 = Start.Y + (ControlPoint.Y - Start.Y) * 2 / 3;
-
-                return new PointF(x1, y1);
-            }
-        }
-
-        private PointF SecondControlPoint
-        {
-            get
-            {
-                var x2 = ControlPoint.X + (End.X - ControlPoint.X) / 3;
-                var y2 = ControlPoint.Y + (End.Y - ControlPoint.Y) / 3;
-
-                return new PointF(x2, y2);
-            }
-        }
-
-        public SvgQuadraticCurveSegment(PointF start, PointF controlPoint, PointF end)
-            : base(start, end)
+        public SvgQuadraticCurveSegment(bool isRelative, PointF controlPoint, PointF end)
+            : base(isRelative, end)
         {
             ControlPoint = controlPoint;
         }
 
-        public override void AddToPath(GraphicsPath graphicsPath)
+        public SvgQuadraticCurveSegment(bool isRelative, PointF end)
+            : this(isRelative, NaN, end)
         {
-            graphicsPath.AddBezier(Start, FirstControlPoint, SecondControlPoint, End);
+        }
+
+        private static PointF CalculateFirstControlPoint(PointF start, PointF controlPoint)
+        {
+            var x1 = start.X + (controlPoint.X - start.X) * 2 / 3;
+            var y1 = start.Y + (controlPoint.Y - start.Y) * 2 / 3;
+
+            return new PointF(x1, y1);
+        }
+
+        private static PointF CalculateSecondControlPoint(PointF controlPoint, PointF end)
+        {
+            var x2 = controlPoint.X + (end.X - controlPoint.X) / 3;
+            var y2 = controlPoint.Y + (end.Y - controlPoint.Y) / 3;
+
+            return new PointF(x2, y2);
+        }
+
+        public override PointF AddToPath(GraphicsPath graphicsPath, PointF start, SvgPathSegmentList parent)
+        {
+            var controlPoint = ControlPoint;
+            if (float.IsNaN(controlPoint.X) || float.IsNaN(controlPoint.Y))
+            {
+                var prev = parent.IndexOf(this) - 1;
+                if (prev >= 0 && parent[prev] is SvgQuadraticCurveSegment prevSegment)
+                {
+                    var prevStart = graphicsPath.PathPoints[graphicsPath.PointCount - 4];
+                    var prevControlPoint = ToAbsolute(prevSegment.ControlPoint, prevSegment.IsRelative, prevStart);
+                    controlPoint = Reflect(prevControlPoint, start);
+                }
+                else
+                    controlPoint = start;
+            }
+            else
+                controlPoint = ToAbsolute(controlPoint, IsRelative, start);
+
+            var end = ToAbsolute(End, IsRelative, start);
+            graphicsPath.AddBezier(start, CalculateFirstControlPoint(start, controlPoint), CalculateSecondControlPoint(controlPoint, end), end);
+            return end;
         }
 
         public override string ToString()
         {
-            return "Q" + ControlPoint.ToSvgString() + " " + End.ToSvgString();
+            if (float.IsNaN(ControlPoint.X) || float.IsNaN(ControlPoint.Y))
+                return (IsRelative ? "t" : "T") + End.ToSvgString();
+            else
+                return (IsRelative ? "q" : "Q") + ControlPoint.ToSvgString() + " " + End.ToSvgString();
+        }
+
+        [System.Obsolete("Use new constructor.")]
+        public SvgQuadraticCurveSegment(PointF start, PointF controlPoint, PointF end)
+            : this(false, controlPoint, end)
+        {
+            Start = start;
+        }
+        [System.Obsolete("Use new AddToPath.")]
+        public override void AddToPath(GraphicsPath graphicsPath)
+        {
+            AddToPath(graphicsPath, Start, null);
         }
     }
 }

Source/Paths/CoordinateParser.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Globalization;
 using Svg.Helpers;
 
 namespace Svg

Source/Paths/SvgArcSegment.cs
@@ -1,7 +1,6 @@
-using System;
+﻿using System;
 using System.Drawing;
 using System.Drawing.Drawing2D;
-using System.Globalization;
 
 namespace Svg.Pathing
 {
@@ -20,8 +19,8 @@ namespace Svg.Pathing
 
         public SvgArcSize Size { get; set; }
 
-        public SvgArcSegment(PointF start, float radiusX, float radiusY, float angle, SvgArcSize size, SvgArcSweep sweep, PointF end)
-            : base(start, end)
+        public SvgArcSegment(float radiusX, float radiusY, float angle, SvgArcSize size, SvgArcSweep sweep, bool isRelative, PointF end)
+            : base(isRelative, end)
         {
             RadiusX = Math.Abs(radiusX);
             RadiusY = Math.Abs(radiusY);
@@ -43,24 +42,26 @@ namespace Svg.Pathing
             return DoublePI - (ta - tb);
         }
 
-        public override void AddToPath(GraphicsPath graphicsPath)
+        public override PointF AddToPath(GraphicsPath graphicsPath, PointF start, SvgPathSegmentList parent)
         {
-            if (Start == End)
+            var end = ToAbsolute(End, IsRelative, start);
+
+            if (start == end)
             {
-                return;
+                return end;
             }
 
             if (RadiusX == 0.0f && RadiusY == 0.0f)
             {
-                graphicsPath.AddLine(Start, End);
-                return;
+                graphicsPath.AddLine(start, end);
+                return end;
             }
 
             var sinPhi = Math.Sin(Angle * RadiansPerDegree);
             var cosPhi = Math.Cos(Angle * RadiansPerDegree);
 
-            var x1dash = cosPhi * (Start.X - End.X) / 2.0 + sinPhi * (Start.Y - End.Y) / 2.0;
-            var y1dash = -sinPhi * (Start.X - End.X) / 2.0 + cosPhi * (Start.Y - End.Y) / 2.0;
+            var x1dash = cosPhi * (start.X - end.X) / 2.0 + sinPhi * (start.Y - end.Y) / 2.0;
+            var y1dash = -sinPhi * (start.X - end.X) / 2.0 + cosPhi * (start.Y - end.Y) / 2.0;
 
             double root;
             var numerator = RadiusX * RadiusX * RadiusY * RadiusY - RadiusX * RadiusX * y1dash * y1dash - RadiusY * RadiusY * x1dash * x1dash;
@@ -84,8 +85,8 @@ namespace Svg.Pathing
             var cxdash = root * rx * y1dash / ry;
             var cydash = -root * ry * x1dash / rx;
 
-            var cx = cosPhi * cxdash - sinPhi * cydash + (Start.X + End.X) / 2.0;
-            var cy = sinPhi * cxdash + cosPhi * cydash + (Start.Y + End.Y) / 2.0;
+            var cx = cosPhi * cxdash - sinPhi * cydash + (start.X + end.X) / 2.0;
+            var cy = sinPhi * cxdash + cosPhi * cydash + (start.Y + end.Y) / 2.0;
 
             var theta1 = CalculateVectorAngle(1.0, 0.0, (x1dash - cxdash) / rx, (y1dash - cydash) / ry);
             var dtheta = CalculateVectorAngle((x1dash - cxdash) / rx, (y1dash - cydash) / ry, (-x1dash - cxdash) / rx, (-y1dash - cydash) / ry);
@@ -103,8 +104,8 @@ namespace Svg.Pathing
             var delta = dtheta / segments;
             var t = 8.0 / 3.0 * Math.Sin(delta / 4.0) * Math.Sin(delta / 4.0) / Math.Sin(delta / 2.0);
 
-            var startX = Start.X;
-            var startY = Start.Y;
+            var startX = start.X;
+            var startY = start.Y;
 
             for (var i = 0; i < segments; ++i)
             {
@@ -130,13 +131,27 @@ namespace Svg.Pathing
                 startX = (float)endpointX;
                 startY = (float)endpointY;
             }
+
+            return end;
         }
 
         public override string ToString()
         {
             var arcFlag = Size == SvgArcSize.Large ? "1" : "0";
             var sweepFlag = Sweep == SvgArcSweep.Positive ? "1" : "0";
-            return "A" + RadiusX.ToSvgString() + " " + RadiusY.ToSvgString() + " " + Angle.ToSvgString() + " " + arcFlag + " " + sweepFlag + " " + End.ToSvgString();
+            return (IsRelative ? "a" : "A") + RadiusX.ToSvgString() + " " + RadiusY.ToSvgString() + " " + Angle.ToSvgString() + " " + arcFlag + " " + sweepFlag + " " + End.ToSvgString();
+        }
+
+        [Obsolete("Use new constructor.")]
+        public SvgArcSegment(PointF start, float radiusX, float radiusY, float angle, SvgArcSize size, SvgArcSweep sweep, PointF end)
+            : this(radiusX, radiusY, angle, size, sweep, false, end)
+        {
+            Start = start;
+        }
+        [Obsolete("Use new AddToPath.")]
+        public override void AddToPath(GraphicsPath graphicsPath)
+        {
+            AddToPath(graphicsPath, Start, null);
         }
     }
 

Source/Paths/SvgClosePathSegment.cs
@@ -1,31 +1,43 @@
+﻿using System.Drawing;
 using System.Drawing.Drawing2D;
 
 namespace Svg.Pathing
 {
     public sealed class SvgClosePathSegment : SvgPathSegment
     {
-        public override void AddToPath(GraphicsPath graphicsPath)
+        public SvgClosePathSegment(bool isRelative)
+            : base(isRelative)
         {
-            var pathData = graphicsPath.PathData;
+        }
 
-            if (pathData.Points.Length > 0)
-            {
-                // Important for custom line caps. Force the path the close with an explicit line, not just an implicit close of the figure.
-                var last = pathData.Points.Length - 1;
-                if (!pathData.Points[0].Equals(pathData.Points[last]))
+        public override PointF AddToPath(GraphicsPath graphicsPath, PointF start, SvgPathSegmentList parent)
+        {
+            graphicsPath.CloseFigure();
+
+            var end = start;
+            for (var i = graphicsPath.PointCount - 1; i >= 0; --i)
+                if ((graphicsPath.PathTypes[i] & 0x7) == 0)
                 {
-                    var i = last;
-                    while (i > 0 && pathData.Types[i] > 0) --i;
-                    graphicsPath.AddLine(pathData.Points[last], pathData.Points[i]);
+                    end = graphicsPath.PathPoints[i];
+                    break;
                 }
-
-                graphicsPath.CloseFigure();
-            }
+            return end;
         }
 
         public override string ToString()
         {
-            return "z";
+            return IsRelative ? "z" : "Z";
+        }
+
+        [System.Obsolete("Use new constructor.")]
+        public SvgClosePathSegment()
+            : this(true)
+        {
+        }
+        [System.Obsolete("Use new AddToPath.")]
+        public override void AddToPath(GraphicsPath graphicsPath)
+        {
+            AddToPath(graphicsPath, Start, null);
         }
     }
 }

Source/Paths/SvgCubicCurveSegment.cs
@@ -8,21 +8,58 @@ namespace Svg.Pathing
         public PointF FirstControlPoint { get; set; }
         public PointF SecondControlPoint { get; set; }
 
-        public SvgCubicCurveSegment(PointF start, PointF firstControlPoint, PointF secondControlPoint, PointF end)
-            : base(start, end)
+        public SvgCubicCurveSegment(bool isRelative, PointF firstControlPoint, PointF secondControlPoint, PointF end)
+            : base(isRelative, end)
         {
             FirstControlPoint = firstControlPoint;
             SecondControlPoint = secondControlPoint;
         }
 
-        public override void AddToPath(GraphicsPath graphicsPath)
+        public SvgCubicCurveSegment(bool isRelative, PointF secondControlPoint, PointF end)
+            : this(isRelative, NaN, secondControlPoint, end)
         {
-            graphicsPath.AddBezier(Start, FirstControlPoint, SecondControlPoint, End);
+        }
+
+        public override PointF AddToPath(GraphicsPath graphicsPath, PointF start, SvgPathSegmentList parent)
+        {
+            var firstControlPoint = FirstControlPoint;
+            if (float.IsNaN(firstControlPoint.X) || float.IsNaN(firstControlPoint.Y))
+            {
+                var prev = parent.IndexOf(this) - 1;
+                if (prev >= 0 && parent[prev] is SvgCubicCurveSegment)
+                {
+                    var prevSecondControlPoint = graphicsPath.PathPoints[graphicsPath.PointCount - 2];
+                    firstControlPoint = Reflect(prevSecondControlPoint, start);
+                }
+                else
+                    firstControlPoint = start;
+            }
+            else
+                firstControlPoint = ToAbsolute(firstControlPoint, IsRelative, start);
+
+            var end = ToAbsolute(End, IsRelative, start);
+            graphicsPath.AddBezier(start, firstControlPoint, ToAbsolute(SecondControlPoint, IsRelative, start), end);
+            return end;
         }
 
         public override string ToString()
         {
-            return "C" + FirstControlPoint.ToSvgString() + " " + SecondControlPoint.ToSvgString() + " " + End.ToSvgString();
+            if (float.IsNaN(FirstControlPoint.X) || float.IsNaN(FirstControlPoint.Y))
+                return (IsRelative ? "s" : "S") + SecondControlPoint.ToSvgString() + " " + End.ToSvgString();
+            else
+                return (IsRelative ? "c" : "C") + FirstControlPoint.ToSvgString() + " " + SecondControlPoint.ToSvgString() + " " + End.ToSvgString();
+        }
+
+        [System.Obsolete("Use new constructor.")]
+        public SvgCubicCurveSegment(PointF start, PointF firstControlPoint, PointF secondControlPoint, PointF end)
+            : this(false, firstControlPoint, secondControlPoint, end)
+        {
+            Start = start;
+        }
+        [System.Obsolete("Use new AddToPath.")]
+        public override void AddToPath(GraphicsPath graphicsPath)
+        {
+            AddToPath(graphicsPath, Start, null);
         }
     }
 }

Source/Paths/SvgLineSegment.cs
@@ -5,19 +5,38 @@ namespace Svg.Pathing
 {
     public sealed class SvgLineSegment : SvgPathSegment
     {
-        public SvgLineSegment(PointF start, PointF end)
-            : base(start, end)
+        public SvgLineSegment(bool isRelative, PointF end)
+            : base(isRelative, end)
         {
         }
 
-        public override void AddToPath(GraphicsPath graphicsPath)
+        public override PointF AddToPath(GraphicsPath graphicsPath, PointF start, SvgPathSegmentList parent)
         {
-            graphicsPath.AddLine(Start, End);
+            var end = ToAbsolute(End, IsRelative, start);
+            graphicsPath.AddLine(start, end);
+            return end;
         }
 
         public override string ToString()
         {
-            return "L" + End.ToSvgString();
+            if (float.IsNaN(End.Y))
+                return (IsRelative ? "h" : "H") + End.X.ToSvgString();
+            else if (float.IsNaN(End.X))
+                return (IsRelative ? "v" : "V") + End.Y.ToSvgString();
+            else
+                return (IsRelative ? "l" : "L") + End.ToSvgString();
+        }
+
+        [System.Obsolete("Use new constructor.")]
+        public SvgLineSegment(PointF start, PointF end)
+            : this(false, end)
+        {
+            Start = start;
+        }
+        [System.Obsolete("Use new AddToPath.")]
+        public override void AddToPath(GraphicsPath graphicsPath)
+        {
+            AddToPath(graphicsPath, Start, null);
         }
     }
 }

Source/Paths/SvgMoveToSegment.cs
@@ -5,19 +5,32 @@ namespace Svg.Pathing
 {
     public class SvgMoveToSegment : SvgPathSegment
     {
-        public SvgMoveToSegment(PointF moveTo)
-            : base(moveTo, moveTo)
+        public SvgMoveToSegment(bool isRelative, PointF moveTo)
+            : base(isRelative, moveTo)
         {
         }
 
-        public override void AddToPath(GraphicsPath graphicsPath)
+        public override PointF AddToPath(GraphicsPath graphicsPath, PointF start, SvgPathSegmentList parent)
         {
             graphicsPath.StartFigure();
+            return ToAbsolute(End, IsRelative, start);
         }
 
         public override string ToString()
         {
-            return "M" + Start.ToSvgString();
+            return (IsRelative ? "m" : "M") + End.ToSvgString();
+        }
+
+        [System.Obsolete("Use new constructor.")]
+        public SvgMoveToSegment(PointF moveTo)
+            : this(false, moveTo)
+        {
+            Start = moveTo;
+        }
+        [System.Obsolete("Use new AddToPath.")]
+        public override void AddToPath(GraphicsPath graphicsPath)
+        {
+            AddToPath(graphicsPath, Start, null);
         }
     }
 }

Source/Paths/SvgPath.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing.Drawing2D;
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
 using Svg.Pathing;
 
 namespace Svg
@@ -50,8 +51,9 @@ namespace Svg
 
                 if (PathData != null && PathData.Count > 0 && PathData.First is SvgMoveToSegment)
                 {
+                    var start = PointF.Empty;
                     foreach (var segment in PathData)
-                        segment.AddToPath(_path);
+                        start = segment.AddToPath(_path, start, PathData);
 
                     if (_path.PointCount == 0)
                     {

commit 85578247d19adcc89cdc38a88b36c0c57d20dade

Fix: #932 'The 'M, q, t, t, t, t, z' path data rendering is broken in master after PR #925' issue.

Source/Paths/SvgQuadraticCurveSegment.cs
@@ -34,16 +34,25 @@ namespace Svg.Pathing
             return new PointF(x2, y2);
         }
 
+        private static PointF CalculateControlPoint(PointF start, PointF firstControlPoint)
+        {
+            var x1 = (firstControlPoint.X * 3 - start.X) / 2;
+            var y1 = (firstControlPoint.Y * 3 - start.Y) / 2;
+
+            return new PointF(x1, y1);
+        }
+
         public override PointF AddToPath(GraphicsPath graphicsPath, PointF start, SvgPathSegmentList parent)
         {
             var controlPoint = ControlPoint;
             if (float.IsNaN(controlPoint.X) || float.IsNaN(controlPoint.Y))
             {
                 var prev = parent.IndexOf(this) - 1;
-                if (prev >= 0 && parent[prev] is SvgQuadraticCurveSegment prevSegment)
+                if (prev >= 0 && parent[prev] is SvgQuadraticCurveSegment)
                 {
                     var prevStart = graphicsPath.PathPoints[graphicsPath.PointCount - 4];
-                    var prevControlPoint = ToAbsolute(prevSegment.ControlPoint, prevSegment.IsRelative, prevStart);
+                    var prevFirstControlPoint = graphicsPath.PathPoints[graphicsPath.PointCount - 3];
+                    var prevControlPoint = CalculateControlPoint(prevStart, prevFirstControlPoint);
                     controlPoint = Reflect(prevControlPoint, start);
                 }
                 else