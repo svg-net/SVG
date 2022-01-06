cf2dfed5fc6f358b4b6d236bdd673208f38ec6e0
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-01-06 19:05:46 +0100

    Split SvgTile

---
+++ Source/Filter Effects/feTile/SvgTile.Drawing.cs
@@ -0,0 +1,13 @@
+﻿#if !NO_SDC
+namespace Svg.FilterEffects
+{
+    public partial class SvgTile : SvgFilterPrimitive
+    {
+        public override void Process(ImageBuffer buffer)
+        {
+            // TODO: Implement feTile filter Process().
+            buffer[Result] = buffer[Input];
+        }
+    }
+}
+#endif
