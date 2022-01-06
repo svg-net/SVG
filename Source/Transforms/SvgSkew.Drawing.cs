d128393c20967ddff14684ae5f9c6734ecc14d07
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-01-06 22:50:06 +0100

    Split SvgSkew

---
+++ Source/Transforms/SvgSkew.Drawing.cs
@@ -0,0 +1,20 @@
+﻿#if !NO_SDC
+using System;
+using System.Drawing.Drawing2D;
+
+namespace Svg.Transforms
+{
+    public sealed partial class SvgSkew : SvgTransform
+    {
+        public override Matrix Matrix
+        {
+            get
+            {
+                var matrix = new Matrix();
+                matrix.Shear((float)Math.Tan(AngleX / 180f * Math.PI), (float)Math.Tan(AngleY / 180f * Math.PI));
+                return matrix;
+            }
+        }
+    }
+}
+#endif
