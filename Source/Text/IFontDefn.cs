93be4dfecb35aeb9108f0559c949684aed8048a0
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-01-06 22:01:25 +0100

    Make entire IFontDefn interface under NO_SDC if/def

--- Source/Text/IFontDefn.cs
+++ Source/Text/IFontDefn.cs
@@ -1,23 +1,22 @@
-﻿using System;
+﻿#if !NO_SDC
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Drawing;
-#if !NO_SDC
 using System.Drawing.Drawing2D;
-#endif
 
 namespace Svg
 {
     public interface IFontDefn : IDisposable
     {
-#if !NO_SDC
         float Size { get; }
         float SizeInPoints { get; }
         void AddStringToPath(ISvgRenderer renderer, GraphicsPath path, string text, PointF location);
         float Ascent(ISvgRenderer renderer);
         IList<RectangleF> MeasureCharacters(ISvgRenderer renderer, string text);
         SizeF MeasureString(ISvgRenderer renderer, string text);
-#endif
+
     }
 }
+#endif
