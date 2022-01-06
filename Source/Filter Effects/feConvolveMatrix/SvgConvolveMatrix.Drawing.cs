51584a57b68176a40b4ca46a98eada6910d80ed0
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-01-06 19:03:31 +0100

    Split SvgConvolveMatrix

---
+++ Source/Filter Effects/feConvolveMatrix/SvgConvolveMatrix.Drawing.cs
@@ -0,0 +1,13 @@
+﻿#if !NO_SDC
+namespace Svg.FilterEffects
+{
+    public partial class SvgConvolveMatrix : SvgFilterPrimitive
+    {
+        public override void Process(ImageBuffer buffer)
+        {
+            // TODO: Implement feConvolveMatrix filter Process().
+            buffer[Result] = buffer[Input];
+        }
+    }
+}
+#endif
