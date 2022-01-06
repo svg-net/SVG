e27c58e108ac0e1efb5ae62203a8bc453aff87c7
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-01-06 22:49:37 +0100

    Split SvgRotate

--- Source/Transforms/SvgRotate.cs
+++ Source/Transforms/SvgRotate.cs
@@ -1,31 +1,12 @@
-#if !NO_SDC
-using System.Drawing.Drawing2D;
-#endif
-using System.Globalization;
-
 namespace Svg.Transforms
 {
-    public sealed class SvgRotate : SvgTransform
+    public sealed partial class SvgRotate : SvgTransform
     {
         public float Angle { get; set; }
 
         public float CenterX { get; set; }
 
         public float CenterY { get; set; }
-
-#if !NO_SDC
-        public override Matrix Matrix
-        {
-            get
-            {
-                var matrix = new Matrix();
-                matrix.Translate(CenterX, CenterY);
-                matrix.Rotate(Angle);
-                matrix.Translate(-CenterX, -CenterY);
-                return matrix;
-            }
-        }
-#endif
 
         public override string WriteToString()
         {
