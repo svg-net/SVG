a1e5e960356c871314a3e01e6636dc150bae8afb
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-01-06 22:41:49 +0100

    Split SvgTransform

---
+++ Source/Transforms/SvgTransform.Drawing.cs
@@ -0,0 +1,43 @@
+#if !NO_SDC
+using System.Drawing.Drawing2D;
+
+namespace Svg.Transforms
+{
+    public abstract partial class SvgTransform
+    {
+        public abstract Matrix Matrix { get; }
+
+        #region Equals implementation
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as SvgTransform;
+            if (other == null)
+                return false;
+
+            return Matrix.Equals(other.Matrix);
+        }
+
+        public override int GetHashCode()
+        {
+            return Matrix.GetHashCode();
+        }
+
+        public static bool operator ==(SvgTransform lhs, SvgTransform rhs)
+        {
+            if (ReferenceEquals(lhs, rhs))
+                return true;
+            if (ReferenceEquals(lhs, null) || ReferenceEquals(rhs, null))
+                return false;
+            return lhs.Equals(rhs);
+        }
+
+        public static bool operator !=(SvgTransform lhs, SvgTransform rhs)
+        {
+            return !(lhs == rhs);
+        }
+
+        #endregion
+    }
+}
+#endif
