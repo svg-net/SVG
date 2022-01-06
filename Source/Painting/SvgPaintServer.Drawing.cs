251bd95e8682f2b642d3c449d58ae9db6aa3604b
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-01-06 20:21:39 +0100

    Split SvgPaintServer

---
+++ Source/Painting/SvgPaintServer.Drawing.cs
@@ -0,0 +1,27 @@
+#if !NO_SDC
+using System.Drawing;
+
+namespace Svg
+{
+    public abstract partial class SvgPaintServer : SvgElement
+    {
+        /// <summary>
+        /// Renders the <see cref="SvgElement"/> and contents to the specified <see cref="ISvgRenderer"/> object.
+        /// </summary>
+        /// <param name="renderer">The <see cref="ISvgRenderer"/> object to render to.</param>
+        protected override void Render(ISvgRenderer renderer)
+        {
+            // Never render paint servers or their children
+        }
+
+        /// <summary>
+        /// Gets a <see cref="Brush"/> representing the current paint server.
+        /// </summary>
+        /// <param name="styleOwner">The owner <see cref="SvgVisualElement"/>.</param>
+        /// <param name="renderer">The renderer object.</param>
+        /// <param name="opacity">The opacity of the brush.</param>
+        /// <param name="forStroke">Not used.</param>
+        public abstract Brush GetBrush(SvgVisualElement styleOwner, ISvgRenderer renderer, float opacity, bool forStroke = false);
+    }
+}
+#endif
