e528424288fdb7d8c9b5851e8fb1d920ff42aaf5
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-01-06 15:20:56 +0100

    Split SvgPolyline

--- Source/Basic Shapes/SvgPolyline.cs
+++ Source/Basic Shapes/SvgPolyline.cs
@@ -1,10 +1,3 @@
-using System;
-using System.Drawing;
-#if !NO_SDC
-using System.Drawing.Drawing2D;
-#endif
-using System.Diagnostics;
-
 namespace Svg
 {
     /// <summary>
@@ -13,51 +6,6 @@ namespace Svg
     [SvgElement("polyline")]
     public partial class SvgPolyline : SvgPolygon
     {
-#if !NO_SDC
-        private GraphicsPath _Path;
-
-        public override GraphicsPath Path(ISvgRenderer renderer)
-        {
-            if (_Path == null || this.IsPathDirty)
-            {
-                _Path = new GraphicsPath();
-
-                try
-                {
-                    for (int i = 0; (i + 1) < Points.Count; i += 2)
-                    {
-                        PointF endPoint = new PointF(Points[i].ToDeviceValue(renderer, UnitRenderingType.Horizontal, this),
-                            Points[i + 1].ToDeviceValue(renderer, UnitRenderingType.Vertical, this));
-
-                        if (renderer == null)
-                        {
-                            var radius = base.StrokeWidth / 2;
-                            _Path.AddEllipse(endPoint.X - radius, endPoint.Y - radius, 2 * radius, 2 * radius);
-                            continue;
-                        }
-
-                        // TODO: Remove unrequired first line
-                        if (_Path.PointCount == 0)
-                        {
-                            _Path.AddLine(endPoint, endPoint);
-                        }
-                        else
-                        {
-                            _Path.AddLine(_Path.GetLastPoint(), endPoint);
-                        }
-                    }
-                }
-                catch (Exception exc)
-                {
-                    Trace.TraceError("Error rendering points: " + exc.Message);
-                }
-                if (renderer != null)
-                    this.IsPathDirty = false;
-            }
-            return _Path;
-        }
-#endif
-
         public override SvgElement DeepCopy()
         {
             return DeepCopy<SvgPolyline>();
