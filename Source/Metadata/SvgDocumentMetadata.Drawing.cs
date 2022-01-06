68ad87f3802b15e10d05c3b466139ac31ce73fe2
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-01-06 19:09:31 +0100

    Split SvgDocumentMetadata

---
+++ Source/Metadata/SvgDocumentMetadata.Drawing.cs
@@ -0,0 +1,16 @@
+﻿#if !NO_SDC
+namespace Svg
+{
+    public partial class SvgDocumentMetadata : SvgElement
+    {
+        /// <summary>
+        /// Renders the <see cref="SvgElement"/> and contents to the specified <see cref="ISvgRenderer"/> object.
+        /// </summary>
+        /// <param name="renderer">The <see cref="ISvgRenderer"/> object to render to.</param>
+        protected override void Render(ISvgRenderer renderer)
+        {
+            // Do nothing. Children should NOT be rendered.
+        }
+    }
+}
+#endif
