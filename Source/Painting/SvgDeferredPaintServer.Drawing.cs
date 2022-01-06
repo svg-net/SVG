657eac47e6b445bb3110e1997927e6995407ceec
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-01-06 21:24:13 +0100

    Split SvgDeferredPaintServer

---
+++ Source/Painting/SvgDeferredPaintServer.Drawing.cs
@@ -0,0 +1,15 @@
+﻿#if !NO_SDC
+using System.Drawing;
+
+namespace Svg
+{
+    public partial class SvgDeferredPaintServer : SvgPaintServer
+    {
+        public override Brush GetBrush(SvgVisualElement styleOwner, ISvgRenderer renderer, float opacity, bool forStroke = false)
+        {
+            EnsureServer(styleOwner);
+            return _concreteServer?.GetBrush(styleOwner, renderer, opacity, forStroke) ?? _fallbackServer?.GetBrush(styleOwner, renderer, opacity, forStroke) ?? NotSet?.GetBrush(styleOwner, renderer, opacity, forStroke);
+        }
+    }
+}
+#endif
