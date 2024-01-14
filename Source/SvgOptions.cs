d5e50590c0a6fe04fdef6d4ab7c80d14569bf4d9
Thomas Stocker <thomas.stocker@gmail.com>
2024-01-14 05:12:38 +0100

    Css parameter follow up (#1110)
    * make adding string css parameter binary compatible
    
    * added release notes
    
    * introduce svgoptions for css paramater
    
    * added obsolete to unnecessary overloads
    
    * added more svgoptions overloads
    
    * fixing build
    
    * Improved release notes

---
+++ Source/SvgOptions.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+#nullable enable
+
+namespace Svg;
+
+public class SvgOptions
+{
+    public SvgOptions()
+    {
+    }
+    
+    public SvgOptions(Dictionary<string, string> entities)
+    {
+        Entities = entities;
+    }
+
+    public SvgOptions(Dictionary<string, string> entities, string css)
+    {
+        Entities = entities;
+        Css = css;
+    }
+
+    public SvgOptions(string css)
+    {
+        Css = css;
+    }
+
+    public Dictionary<string, string>? Entities { get; set; }
+    public string? Css { get; set; }
+}
